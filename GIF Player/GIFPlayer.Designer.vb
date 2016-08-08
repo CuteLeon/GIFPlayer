0767573a1b1f460362da2c12419ede29a1e73efa
Leon <xiaoyankeji@hotmail.com>
2016-08-08 14:00:00 +0800

    可以获取每一帧的时间间隔

--- GIF Player/GIFPlayer.Designer.vb
+++ GIF Player/GIFPlayer.Designer.vb
@@ -29,11 +29,11 @@ Partial Class GIFPlayer
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(38, 226)
+        Me.Button1.Location = New System.Drawing.Point(206, 12)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(75, 336)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "开始"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'PictureBox1
@@ -41,7 +41,7 @@ Partial Class GIFPlayer
         Me.PictureBox1.Image = Global.GIF_Player.My.Resources.Resource.GIFTest
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(231, 220)
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 360)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
@@ -49,7 +49,7 @@ Partial Class GIFPlayer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(290, 266)
+        Me.ClientSize = New System.Drawing.Size(290, 360)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "GIFPlayer"
--- GIF Player/GIFPlayer.vb
+++ GIF Player/GIFPlayer.vb
@@ -10,17 +10,31 @@ Public Class GIFPlayer
         Dim FormGraphics As Graphics = Me.CreateGraphics
         Dim GIFBitmap As Bitmap = My.Resources.Resource.GIFTest
         Dim GIFFrame As Guid()
-        Debug.Print("1")
         GIFFrame = GIFBitmap.FrameDimensionsList()
         Dim TempFrame As New FrameDimension(GIFFrame(0))
         Dim FrameCount As Integer = GIFBitmap.GetFrameCount(TempFrame)
-        Debug.Print("2")
+        Dim PropertyIndex As Integer = 0
+        Dim IntervalBytes(3) As Byte
+        Dim TimeInterval As Integer
+        Dim IntervalProperty As PropertyItem = Nothing
         For Index As Integer = 0 To FrameCount - 1
             GIFBitmap.SelectActiveFrame(TempFrame, Index)
+            For PropertyIndex = 0 To GIFBitmap.PropertyIdList.Length - 1
+                If GIFBitmap.PropertyIdList.GetValue(PropertyIndex) = 20736 Then 'ID为20736的属性保存的是时间间隔
+                    IntervalProperty = GIFBitmap.PropertyItems.GetValue(PropertyIndex)
+                    IntervalBytes(0) = IntervalProperty.Value(0 + Index * 4)
+                    IntervalBytes(1) = IntervalProperty.Value(1 + Index * 4)
+                    IntervalBytes(2) = IntervalProperty.Value(2 + Index * 4)
+                    IntervalBytes(3) = IntervalProperty.Value(3 + Index * 4)
+                    TimeInterval = BitConverter.ToInt32(IntervalBytes, 0) * 10
+                    Exit For
+                End If
+            Next
             FormGraphics.DrawImage(GIFBitmap, 0, 0)
             '经过Graphics.DrawImage后的就是GIF的分解帧
+            'TimeInterval是每一帧的时间间隔
             Me.Refresh()
-            Threading.Thread.Sleep(10)
+            Threading.Thread.Sleep(TimeInterval)
         Next
         Debug.Print("3")
     End Sub
